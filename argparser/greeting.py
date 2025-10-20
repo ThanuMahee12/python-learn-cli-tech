044627f9e212e04ce8ee004471fb04de92508252
ThanuMahee12 <mtthanu126@gmail.com>
2025-10-20 10:14:09 -0400

    refactor:argparser refactor with new parameter  and git ignore

--- argparser/greeting.py
+++ argparser/greeting.py
@@ -2,5 +2,6 @@ import argparse
 
 argparser = argparse.ArgumentParser(description="Greet the user.")
 argparser.add_argument("name", type=str, help="The name of the user to greet.")
+argparser.add_argument("age",type=int, help="Enter the age")
 args = argparser.parse_args()
-print(f"Hello, {args.name}!, welcome to the program.")
+print(f"Hello, {args.name}!, welcome to the program.\nyour age is {args.age}")
